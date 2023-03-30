6232cc15b66f80977a9aff8dbf805f9e98709a4d
ChronoDan07 <chronodan17@gmail.com>
2023-03-30 10:55:00 +0300

    Edit rps.js to correspond with the updates of the new UI

--- rps.js
+++ rps.js
@@ -6,107 +6,147 @@ function getComputerChoice(){
     return chosen;
 }
 
-function insensitive(){
-    const sen = document.getElementById("shape").value;
-    const ssen = sen.toLowerCase();
-    const insen = ssen[0].toUpperCase() + ssen.substring(1);
-    return insen;
+function playSound(e){
+    const audio = document.querySelector(`audio[data-audio="${e}"]`);
+    if (!audio) return;
+    // audio.currentTime = 0;
+    audio.play();
 }
-let computerScore=0;
-let playerScore=0;
+
+
+let computerScore = 0;
+let playerScore = 0;
 let round = 0;
+const computerChoice = document.getElementById("comp-choice");
+const results = document.getElementById("result");
+const rounds = document.getElementById("rnd");
+const plscores = document.getElementById("plscore");
+const cpscores = document.getElementById("cpscore");
+const finals = document.getElementById("final");
 
+
+//-------------------------------------------------
+const elements = document.querySelectorAll('.Rock, .Paper, .Scissors');
+let clickedItem;
+elements.forEach(element => {
+ element.addEventListener('click', function(){
+    const clickedItem = this.classList.value;
+    console.log(clickedItem);
+    playGame(clickedItem);
+ })   
+});
+//-------------------------------------------------
 function resetGame(){
-    document.getElementById("comp-choice").innerHTML = "----";
-    document.getElementById("result").innerHTML ="----";
-    document.getElementById("final").innerHTML = "______________";
-    computerScore=0;
-    playerScore=0;
-    round=0;
-    document.getElementById("rnd").innerHTML ="0";
-    document.getElementById("plscore").innerHTML ="0";
-    document.getElementById("cpscore").innerHTML ="0";
-    document.getElementById("final").style.color="darkgreen";
-    document.getElementById("result").style.color="darkgreen";
-
+    computerChoice.textContent = "----";
+    results.textContent = "----";
+    finals.textContent = "______________";
+    computerScore = 0;
+    playerScore = 0;
+    round = 0;
+    rounds.textContent = "0";
+    plscores.textContent = "0";
+    cpscores.textContent = "0";
+    document.getElementById("final").style.color = "darkgreen";
+    document.getElementById("result").style.color = "darkgreen";
 }
 
-function playRound(){
-let playerSelection=insensitive();
+function checkWinner() {
+
+    if (playerScore === 5 || computerScore === 5) {
+        if (computerScore === 5) {
+            finals.textContent = "You lose the game!";
+            document.getElementById("final").style.color="red";
+            playSound('biglose');
+        }
+        else if (playerScore === 5) {
+            finals.textContent = "You win the game!";
+            playSound('bigwin');
+        }
+        return true;
+    }
+    return false;
+}
+
+
+function playGame(playerSelection){
+
+
 let computerSelection=getComputerChoice();
 
-        if (playerSelection=="Rock" && computerSelection=="Paper"){
+if (checkWinner()) {
+    return;
+}
+
+        if (playerSelection === "Rock" && computerSelection === "Paper"){
             computerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${computerSelection} beats ${playerSelection}, you lose!`;
-            document.getElementById("result").style.color="red";
+            computerChoice.textContent = `The computer chose: ${computerSelection}`;
+            results.textContent = `${computerSelection} beats ${playerSelection}, you lose!`;
+            document.getElementById("result").style.color = "red";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('lose');
         }
-        else if (playerSelection=="Rock" && computerSelection=="Scissors"){
+        else if (playerSelection === "Rock" && computerSelection === "Scissors"){
             playerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${playerSelection} beats ${computerSelection}, you win!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent =`${playerSelection} beats ${computerSelection}, you win!`;
             document.getElementById("result").style.color="darkgreen";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('rwin');
         }
 
-        else if (playerSelection=="Paper" && computerSelection=="Scissors"){
+        else if (playerSelection === "Paper" && computerSelection === "Scissors"){
             computerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${computerSelection} beats ${playerSelection}, you lose!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent =`${computerSelection} beats ${playerSelection}, you lose!`;
             document.getElementById("result").style.color="red";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('lose');
         }
 
-        else if (playerSelection=="Paper" && computerSelection=="Rock"){
+        else if (playerSelection === "Paper" && computerSelection === "Rock"){
             playerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${playerSelection} beats ${computerSelection}, you win!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent =`${playerSelection} beats ${computerSelection}, you win!`;
             document.getElementById("result").style.color="darkgreen";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('pwin');
         }
 
-        else if (playerSelection=="Scissors" && computerSelection=="Rock"){
+        else if (playerSelection === "Scissors" && computerSelection === "Rock"){
             computerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${computerSelection} beats ${playerSelection}, you lose!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent =`${computerSelection} beats ${playerSelection}, you lose!`;
             document.getElementById("result").style.color="red";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('lose');
         }
 
-        else if (playerSelection=="Scissors" && computerSelection=="Paper"){
+        else if (playerSelection === "Scissors" && computerSelection === "Paper"){
             playerScore++;
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML =`${playerSelection} beats ${computerSelection}, you win!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent =`${playerSelection} beats ${computerSelection}, you win!`;
             document.getElementById("result").style.color="darkgreen";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('swin');
         }
 
         else if (playerSelection === computerSelection){
-            document.getElementById("comp-choice").innerHTML =`The computer chose: ${computerSelection}`;
-            document.getElementById("result").innerHTML = `It's a draw!`;
+            computerChoice.textContent =`The computer chose: ${computerSelection}`;
+            results.textContent = `It's a draw!`;
             document.getElementById("result").style.color="gray";
             round++;
-            document.getElementById("rnd").innerHTML = round;
+            rounds.textContent = round;
+            // playSound('draw');
         }
-    document.getElementById("plscore").innerHTML = playerScore;
-    document.getElementById("cpscore").innerHTML = computerScore;
-
-    if (playerScore == 5 || computerScore == 5) {
-            if (computerScore == 5) {
-                document.getElementById("final").innerHTML = "You lose the game!";
-                document.getElementById("final").style.color="red";
-            }
-            else if (playerScore == 5) {
-                document.getElementById("final").innerHTML = "You win the game!";
-            }
-    }
+    plscores.textContent = playerScore;
+    cpscores.textContent = computerScore;
+    checkWinner();
 }
 
 
